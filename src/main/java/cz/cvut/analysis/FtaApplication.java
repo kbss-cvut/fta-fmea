15e90766f16a73f72fabdbd0016ecf8189092633
Jakub Gruber <jgr@paymorrow.com>
2020-09-20 21:55:45 +0200

    Project setup

---
+++ src/main/java/cz/cvut/analysis/FtaApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtaApplication.class, args);
+	}
+
+}
