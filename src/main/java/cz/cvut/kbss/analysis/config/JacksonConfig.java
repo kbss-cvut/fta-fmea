4864c30609e18b5d83a20aa268c7975ae1ca09ea
Bogdan Kostov <kostobog@fel.cvut.cz>
2020-12-15 12:55:03 +0100

    config ObjectMapper jsonld "SCAN_PACKAGE"

--- src/main/java/cz/cvut/kbss/analysis/config/JacksonConfig.java
+++ src/main/java/cz/cvut/kbss/analysis/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package cz.cvut.kbss.analysis.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.cvut.kbss.jsonld.ConfigParam;
 import cz.cvut.kbss.jsonld.jackson.JsonLdModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,9 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(new JsonLdModule());
+        JsonLdModule m = new JsonLdModule();
+        m.configure(ConfigParam.SCAN_PACKAGE, "cz.cvut.kbss.analysis");
+        mapper.registerModule(m);
         return mapper;
     }
 
