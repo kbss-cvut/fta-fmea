93f9a53f3d3eaddf06746d1e4b22059fd6e8b8ea
Matěj Kulich <matej.kulich99@gmail.com>
2021-08-30 15:32:52 +0200

    Added Vocabulary import

--- src/main/java/cz/cvut/kbss/analysis/dao/FunctionDao.java
+++ src/main/java/cz/cvut/kbss/analysis/dao/FunctionDao.java
@@ -3,6 +3,7 @@ package cz.cvut.kbss.analysis.dao;
 import cz.cvut.kbss.analysis.config.conf.PersistenceConf;
 import cz.cvut.kbss.analysis.model.Component;
 import cz.cvut.kbss.analysis.model.Function;
+import cz.cvut.kbss.analysis.util.Vocabulary;
 import cz.cvut.kbss.jopa.model.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
