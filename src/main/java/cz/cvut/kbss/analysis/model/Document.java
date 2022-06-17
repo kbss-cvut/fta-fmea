8c532f97999b405b6142522ab0bd43debc22b5bc
Bogdan Kostov <kostobog@fel.cvut.cz>
2022-06-17 09:14:20 +0200

    [FIX] Extend model to support systems being documented-in documents and terms referencing annotation source.

---
+++ src/main/java/cz/cvut/kbss/analysis/model/Document.java
@@ -0,0 +1,9 @@
+package cz.cvut.kbss.analysis.model;
+
+import cz.cvut.kbss.analysis.util.Vocabulary;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+
+@OWLClass(iri = Vocabulary.s_c_Document)
+public class Document extends AbstractEntity{
+
+}
