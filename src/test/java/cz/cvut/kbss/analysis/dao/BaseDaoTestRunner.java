6b25341c003228bc20ec9cfaca5eaa347232487b
Bogdan Kostov <kostobog@fel.cvut.cz>
2023-10-31 10:14:47 +0100

    [#44] Upgrading project build - upgrade gradle to gradle 8.4 - upgrade to java 11 - upgrade jopa to latest 1.1.4 - upgrade jopa-spring-transaction to latest 0.2.0 - upgrade jb4jsonld-jackson to latest 0.13.2
    upgrade build plugins
    - upgrade plugin org.springframework.boot 2.7.17 latest version supporting java 11
    - upgrade io.spring.dependency-management to latest 1.1.3
    - upgrade io.freefair.aspectj.post-compile-weaving to latest 8.4

--- src/test/java/cz/cvut/kbss/analysis/dao/BaseDaoTestRunner.java
+++ src/test/java/cz/cvut/kbss/analysis/dao/BaseDaoTestRunner.java
@@ -5,14 +5,14 @@ import cz.cvut.kbss.analysis.config.conf.RepositoryConf;
 import cz.cvut.kbss.analysis.environment.TransactionalTestRunner;
 import cz.cvut.kbss.analysis.environment.config.TestPersistenceConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class, classes = {
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class, classes = {
         TestPersistenceConfig.class,
         PersistenceConf.class,
         RepositoryConf.class,
