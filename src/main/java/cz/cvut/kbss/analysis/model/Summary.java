980db6b1fc92d4da6e1e427651fdde853add896c
Bogdan Kostov <kostobog@fel.cvut.cz>
2023-12-23 19:17:48 +0100

    [Add] Create new entity Summary with SparqlResultSetMapping. - the asEntiy method can be used to convert the summary to other summary types, e.g. FautlTree summary.

---
+++ src/main/java/cz/cvut/kbss/analysis/model/Summary.java
@@ -0,0 +1,31 @@
+package cz.cvut.kbss.analysis.model;
+
+import cz.cvut.kbss.jopa.model.annotations.EntityResult;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.SparqlResultSetMapping;
+import cz.cvut.kbss.jopa.model.annotations.SparqlResultSetMappings;
+
+@SparqlResultSetMappings(
+        @SparqlResultSetMapping(name="Summary", entities = {
+                @EntityResult(entityClass=Summary.class)
+        })
+)
+@OWLClass(iri = "http://onto.fel.cvut.cz/ontologies/fta-fmea-application/summary")
+public class Summary extends NamedEntity {
+        public <T extends NamedEntity> T asEntity(Class<T> cls){
+                T entity = null;
+                try {
+                        entity = cls.getConstructor().newInstance();
+                }catch (Exception e){
+                        throw new RuntimeException(String.format("Entity class %s does not have no-arg constructor", cls.getName()));
+                }
+                copyTo(entity);
+                return entity;
+        }
+
+        public void copyTo(NamedEntity namedEntity){
+                namedEntity.setUri(this.getUri());
+                namedEntity.setName(this.getName());
+                namedEntity.setDescription(this.getDescription());
+        }
+}
