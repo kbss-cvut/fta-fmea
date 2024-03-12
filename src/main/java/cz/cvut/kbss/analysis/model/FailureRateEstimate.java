2e5182f92b57a01d3b1cc904f639d5bc6fd33c18
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-03-12 08:53:51 +0100

    [Fix #65] Extend jopa model add FailureRateEstimate.estimationMethod and FaultEvent.selectedEstimate

--- src/main/java/cz/cvut/kbss/analysis/model/FailureRateEstimate.java
+++ src/main/java/cz/cvut/kbss/analysis/model/FailureRateEstimate.java
@@ -1,8 +1,11 @@
 package cz.cvut.kbss.analysis.model;
 
+import cz.cvut.kbss.analysis.model.method.EstimationMethod;
 import cz.cvut.kbss.analysis.util.Vocabulary;
+import cz.cvut.kbss.jopa.model.annotations.CascadeType;
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
 import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,5 +16,11 @@ public class FailureRateEstimate extends AnalysisProduct{
 
     @OWLDataProperty(iri = Vocabulary.s_p_has_value)
     protected Double value;
+
+    /**
+     * The estimation method
+     */
+    @OWLObjectProperty(iri = Vocabulary.s_p_based_on, cascade = CascadeType.ALL)
+    protected EstimationMethod estimationMethod;
 
 }
