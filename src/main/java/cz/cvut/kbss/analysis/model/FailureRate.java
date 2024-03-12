3ee4f76b55751a62d0ac9dea712b7b1f2f20b22c
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-03-12 09:03:23 +0100

    [Fix #65] Change casecade of FaultEvent and related entities

--- src/main/java/cz/cvut/kbss/analysis/model/FailureRate.java
+++ src/main/java/cz/cvut/kbss/analysis/model/FailureRate.java
@@ -1,6 +1,8 @@
 package cz.cvut.kbss.analysis.model;
 
 import cz.cvut.kbss.analysis.util.Vocabulary;
+import cz.cvut.kbss.jopa.model.annotations.CascadeType;
+import cz.cvut.kbss.jopa.model.annotations.FetchType;
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
 import cz.cvut.kbss.jopa.model.annotations.OWLObjectProperty;
 import lombok.Getter;
@@ -11,13 +13,13 @@ import lombok.Setter;
 @Setter
 public class FailureRate extends AbstractEntity{
 
-    @OWLObjectProperty(iri = Vocabulary.s_p_has_requirement)
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_requirement, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private FailureRateRequirement requirement;
 
-    @OWLObjectProperty(iri = Vocabulary.s_p_has_estimate)
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_estimate, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private FailureRateEstimate estimate;
 
-    @OWLObjectProperty(iri = Vocabulary.s_p_has_prediction)
+    @OWLObjectProperty(iri = Vocabulary.s_p_has_prediction, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private FailureRateEstimate prediction;
 
 }
