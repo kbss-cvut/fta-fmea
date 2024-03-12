547481ecb789e6cb4ea2b0a27d833da3ec6038b9
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-03-12 09:03:40 +0100

    [Fix #65] Add AVA related entities

---
+++ src/main/java/cz/cvut/kbss/analysis/model/ava/IndependentSNSItem.java
@@ -0,0 +1,25 @@
+package cz.cvut.kbss.analysis.model.ava;
+
+import cz.cvut.kbss.analysis.model.Item;
+import cz.cvut.kbss.analysis.util.Vocabulary;
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
+import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * This entity represents a Kind (typeCategory should be set to Kind) of independent Item used as a component in the SNS
+ * partonomy identified by a partNumber and stock.
+ *
+ *
+ */
+@OWLClass(iri = Vocabulary.s_c_reusable_system)
+@Getter
+@Setter
+public class IndependentSNSItem extends Item {
+    @OWLDataProperty(iri = Vocabulary.s_p_part_number)
+    private String partNumber;
+
+    @OWLDataProperty(iri = Vocabulary.s_p_stock)
+    private String stock;
+}
