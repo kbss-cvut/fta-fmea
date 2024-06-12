934cf63c911676663a130dcaab2111130f89fb5b
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-06-12 14:00:23 +0200

    [Fixes #116] Implement communication with analysis module to fetch operational failure rates

--- src/main/java/cz/cvut/kbss/analysis/config/JacksonConfig.java
+++ src/main/java/cz/cvut/kbss/analysis/config/JacksonConfig.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.cvut.kbss.jsonld.ConfigParam;
 import cz.cvut.kbss.jsonld.jackson.JsonLdModule;
 import cz.cvut.kbss.jsonld.jackson.serialization.SerializationConstants;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class JacksonConfig {
@@ -22,4 +24,10 @@ public class JacksonConfig {
         return mapper;
     }
 
+    @Bean(name = "customRestTemplate")
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+//                .additionalMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper()))
+                .build();
+    }
 }
