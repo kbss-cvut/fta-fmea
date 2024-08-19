5c07a29f5c06c6bd47a72f673a7c359aca05fe82
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-08-19 13:24:00 +0200

    Fix failing tests

--- src/test/java/cz/cvut/kbss/analysis/environment/config/TestPersistenceConfig.java
+++ src/test/java/cz/cvut/kbss/analysis/environment/config/TestPersistenceConfig.java
@@ -3,14 +3,19 @@ package cz.cvut.kbss.analysis.environment.config;
 import com.github.ledsoft.jopa.spring.transaction.DelegatingEntityManager;
 import com.github.ledsoft.jopa.spring.transaction.JopaTransactionManager;
 import cz.cvut.kbss.analysis.environment.TestPersistenceFactory;
+import cz.cvut.kbss.analysis.service.IdentifierService;
+import cz.cvut.kbss.analysis.service.security.SecurityUtils;
 import cz.cvut.kbss.jopa.model.EntityManagerFactory;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan(basePackages = "cz.cvut.kbss.analysis.persistence")
-@Import({TestPersistenceFactory.class})
+@Import({TestPersistenceFactory.class, IdentifierService.class, SecurityUtils.class })
 @EnableTransactionManagement
 public class TestPersistenceConfig {
 
