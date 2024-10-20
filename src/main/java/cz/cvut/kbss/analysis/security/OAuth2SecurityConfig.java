f1331c7308c9465badc444ef00fc09a95f2480cd
Bogdan Kostov <kostobog@fel.cvut.cz>
2024-10-20 16:49:26 +0200

    [Fix partially kbss-cvut/23ava-distribution#23] Add configurable role prefix

--- src/main/java/cz/cvut/kbss/analysis/security/OAuth2SecurityConfig.java
+++ src/main/java/cz/cvut/kbss/analysis/security/OAuth2SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -40,6 +41,11 @@ public class OAuth2SecurityConfig {
     private final AuthenticationSuccess authenticationSuccess;
 
     private final SecurityConf config;
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults(){
+        return new GrantedAuthorityDefaults(config.getRolePrefix());
+    }
 
     @Autowired
     public OAuth2SecurityConfig(AuthenticationSuccess authenticationSuccess, SecurityConf config) {
