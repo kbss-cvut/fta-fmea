aa259d063a7f212920116ad8ead495224fb9551c
Bogdan Kostov <kostobog@fel.cvut.cz>
2025-04-02 13:04:40 +0200

    [Fixes partially kbss-cvut/fta-fmea-ui#668] Add config parameter api.base-url for open api

--- src/main/java/cz/cvut/kbss/analysis/config/OpenApiConfig.java
+++ src/main/java/cz/cvut/kbss/analysis/config/OpenApiConfig.java
@@ -6,6 +6,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 /**
  * Configuration class for OpenAPI documentation.
@@ -26,5 +31,28 @@ import io.swagger.v3.oas.annotations.security.SecuritySchemes;
                 in = SecuritySchemeIn.HEADER
         )
 })
+@Configuration
 public class OpenApiConfig {
+        /**
+         * Overwrites OpenAPI server url, i.e. url where REST API is accessible.
+         * This is useful when running the application behind a reverse proxy.
+         *
+         * Whole url should be provided, including protocol and port, e.g.
+         * <code>https://example.com/production/performance-server</code>
+         */
+        @Value("${api.base-url}")
+        private String apiBaseUrl;
+
+        @Bean
+        ForwardedHeaderFilter forwardedHeaderFilter() {
+                return new ForwardedHeaderFilter();
+        }
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        .addServersItem(new Server()
+                                .url(apiBaseUrl)
+                                .description("Production Server"));
+        }
 }
