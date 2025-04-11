f6268d51a4a3f457e42071d815da55deef7f5877
Bogdan Kostov <kostobog@fel.cvut.cz>
2025-04-11 17:12:33 +0200

    [Fixes partially kbss-cvut/fta-fmea-ui#668] Fix swagger configuration - add security scheme to allow authentication with bearer when running behind keycloak - add description how use try it out feature with internal authentication - set version from gradle project

--- src/main/java/cz/cvut/kbss/analysis/config/OpenApiConfig.java
+++ src/main/java/cz/cvut/kbss/analysis/config/OpenApiConfig.java
@@ -1,58 +1,106 @@
 package cz.cvut.kbss.analysis.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 /**
  * Configuration class for OpenAPI documentation.
  */
-@OpenAPIDefinition(
-        info = @Info(
-                title = "FTA and FMEA API",
-                description = "Docs for FTA and FMEA API",
-                version = "1.0"
-        )
-)
-@SecuritySchemes({
-        @SecurityScheme(
-                name = "basicAuth",
-                description = "Basic auth",
-                scheme = "basic",
-                type = SecuritySchemeType.HTTP,
-                in = SecuritySchemeIn.HEADER
-        )
-})
 @Configuration
+@Slf4j
 public class OpenApiConfig {
-        /**
-         * Overwrites OpenAPI server url, i.e. url where REST API is accessible.
-         * This is useful when running the application behind a reverse proxy.
-         *
-         * Whole url should be provided, including protocol and port, e.g.
-         * <code>https://example.com/production/performance-server</code>
-         */
-        @Value("${api.base-url}")
-        private String apiBaseUrl;
+    /**
+     * Application version
+     */
+    @Value("${application.version:development}")
+    private String version;
 
-        @Bean
-        ForwardedHeaderFilter forwardedHeaderFilter() {
-                return new ForwardedHeaderFilter();
-        }
 
-        @Bean
-        public OpenAPI customOpenAPI() {
-                return new OpenAPI()
-                        .addServersItem(new Server()
-                                .url(apiBaseUrl)
-                                .description("Production Server"));
-        }
+    /**
+     * Overwrites OpenAPI server url, i.e. url where REST API is accessible.
+     * This is useful when running the application behind a reverse proxy.
+     * <p>
+     * Whole url should be provided, including protocol and port, e.g.
+     * <code>https://example.com/production/performance-server</code>
+     */
+    @Value("${api.base-url}")
+    private String apiBaseUrl;
+
+    @Value("${security.provider}")
+    private String securityProvider;
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        log.info("Creating OpenAPI - <{}>, {}", apiBaseUrl, securityProvider);
+
+        return switch (securityProvider == null ? "" : securityProvider) {
+            case "internal" -> apiKeyOpenAPI();
+            case "oidc" -> httpOpenAPI();
+            default -> commonOpenAPI();
+        };
+    }
+
+    protected OpenAPI commonOpenAPI() {
+        return new OpenAPI().addServersItem(new Server()
+                        .url(apiBaseUrl)
+                        .description("Production Server"))
+                .info(new io.swagger.v3.oas.models.info.Info().title("FTA and FMEA API")
+                        .description("Docs for FTA and FMEA API.")
+                        .version(version));
+    }
+
+    public OpenAPI customOpenAPI(String schemeKey, SecurityScheme scheme) {
+        return commonOpenAPI().components(new Components()
+                .addSecuritySchemes(schemeKey, scheme))
+        .addSecurityItem(new SecurityRequirement().addList(schemeKey));
+    }
+
+    public OpenAPI apiKeyOpenAPI() {
+        String description = """
+        To use the `Try it out` feature - login (for example using FTA FMEA UI or cURL) and make sure that the returned JSESSIONID cookie is set in the browser window where you run swagger.
+        #### Login Using cURL
+        Linxu:
+        ```
+        curl --location '%sauth/signin' \\
+        --form 'username="..."' \\
+        --form 'password="..."'
+        ```
+        
+        Windows:
+        ```
+        curl -i ^
+        --form username="..." ^
+        --form password="..." ^
+        %s/auth/signin
+        ```
+        """.formatted(apiBaseUrl, apiBaseUrl);
+        OpenAPI openAPI = commonOpenAPI();
+        openAPI.getInfo().setDescription(openAPI.getInfo().getDescription() + "<br/><br/>" + description);
+        return openAPI;
+    }
+
+    public OpenAPI httpOpenAPI() {
+        return customOpenAPI(
+                "bearer-key",
+                new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")
+        );
+    }
 }
